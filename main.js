b6dec2454b70fd1d779def76d1972d95281da0dc
LuisScholze <131001253+LuisScholze@users.noreply.github.com>
2024-08-27 10:55:50 -0300

    kjuk

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-const caixaPrincipal = document.querySelector{"caixa-pricipal"};
-const caixaPrincipal = document.querySelector{"caixa-perguntas"};
-const caixaPrincipal = document.querySelector{"caixa-alternativas"};
-const caixaPrincipal = document.querySelector{"caixa-resultado"};
-const caixaPrincipal = document.querySelector{"texto-resultado"};
+const caixaPrincipal = document.querySelector("caixa-pricipal");
+const caixaPrincipal = document.querySelector("caixa-perguntas");
+const caixaPrincipal = document.querySelector("caixa-alternativas");
+const caixaPrincipal = document.querySelector("caixa-resultado");
+const caixaPrincipal = document.querySelector("texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: ["aternativa1","alternativa2","alternativa3"]
+    },
