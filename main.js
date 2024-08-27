3255d4af0df3daa574413a014f42257b548d25c5
LuisScholze <131001253+LuisScholze@users.noreply.github.com>
2024-08-27 07:46:01 -0300

    fbdhdhdtg

---
+++ main.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector{"caixa-pricipal"};
+const caixaPrincipal = document.querySelector{"caixa-perguntas"};
+const caixaPrincipal = document.querySelector{"caixa-alternativas"};
+const caixaPrincipal = document.querySelector{"caixa-resultado"};
+const caixaPrincipal = document.querySelector{"texto-resultado"};
